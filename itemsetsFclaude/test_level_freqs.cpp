b5d78800af76228fcc575ded4efd9bf142e456e4
Marcello Tavano <chelotavano.udp@gmail.com>
2015-05-22 15:06:54 -0400

    pushing before diego's router kill me

--- itemsetsFclaude/test_level_freqs.cpp
+++ itemsetsFclaude/test_level_freqs.cpp
@@ -6,109 +6,108 @@
 #include <iterator>
 #include <algorithm>
 
-
 using namespace std;
 
 class Node {
-public:
-	map<int, Node*> children;
-	int value, counter;
+ public:
+  map<int, Node *> children;
+  int value, counter;
 
-	Node(int val, int cnt) {
-		value = val;
-		counter = cnt;
-	}
+  Node(int val, int cnt) {
+    value = val;
+    counter = cnt;
+  }
 
-	Node() {
-		value = 0; counter = 0;
-	}
-
+  Node() {
+    value = 0;
+    counter = 0;
+  }
 };
 
 class Tree {
-public:
-	Node *root;
-	Tree() {
-		root = new Node();
-	}
-	void insert(const vector<int> &v) {
-		Node * aux = root;
-		for (const auto &val : v) {
-			if (aux->children[val] == NULL) {
-				aux->children[val] = new Node(val, 0);
-			}
-			aux->children[val]->counter++;
-			aux = aux->children[val];
-		}
-	}
+ public:
+  Node *root;
+  Tree() { root = new Node(); }
+  void insert(const vector<int> &v) {
+    Node *aux = root;
+    for (const auto &val : v) {
+      if (aux->children[val] == NULL) {
+        aux->children[val] = new Node(val, 0);
+      }
+      aux->children[val]->counter++;
+      aux = aux->children[val];
+    }
+  }
 };
 
 Tree build_tree(vector<vector<int>> &data) {
-	Tree t;
-	for (const auto &v : data) {
-		t.insert(v);
-	}
-	return t;
+  Tree t;
+  for (const auto &v : data) {
+    t.insert(v);
+  }
+  return t;
 }
 
 vector<vector<int>> level_values(Tree &t) {
-	int nodeCount = 0;
-	vector<vector<int>> res;
-	queue<Node*> cola;
-	int nivel = -1;
-	cola.push(NULL);
-	cola.push(t.root);
-	while(cola.size() > 1) {
-		Node *n = cola.front(); cola.pop();
-		if (n == NULL) {
-			nivel++;
-			cola.push(NULL);
-			res.push_back(vector<int>());
-		} else {
-			nodeCount++;
-			res[nivel].push_back(n->counter);
-			for( auto v : n->children) {
-				cola.push(v.second);
-			}
-		}
-	}
-	cout << "Node count: " << nodeCount << endl;
-	return res;
+  int nodeCount = 0;
+  vector<vector<int>> res;
+  queue<Node *> cola;
+  int nivel = -1;
+  cola.push(NULL);
+  cola.push(t.root);
+  while (cola.size() > 1) {
+    Node *n = cola.front();
+    cola.pop();
+    if (n == NULL) {
+      nivel++;
+      cola.push(NULL);
+      res.push_back(vector<int>());
+    } else {
+      nodeCount++;
+      res[nivel].push_back(n->counter);
+      for (auto v : n->children) {
+        cola.push(v.second);
+      }
+    }
+  }
+  cout << "Node count: " << nodeCount << endl;
+  return res;
 }
 
 vector<vector<int>> level_labels(Tree &t) {
-	vector<vector<int>> res;
-	queue<Node*> cola;
-	int nivel = -1;
-	cola.push(NULL);
-	cola.push(t.root);
-	while(cola.size() > 1) {
-		Node *n = cola.front(); cola.pop();
-		if (n == NULL) {
-			nivel++;
-			cola.push(NULL);
-			res.push_back(vector<int>());
-		} else {
-			res[nivel].push_back(n->value);
-			for( auto v : n->children) {
-				cola.push(v.second);
-			}
-		}
-	}
-	return res;
+  vector<vector<int>> res;
+  queue<Node *> cola;
+  int nivel = -1;
+  cola.push(NULL);
+  cola.push(t.root);
+  while (cola.size() > 1) {
+    Node *n = cola.front();
+    cola.pop();
+    if (n == NULL) {
+      nivel++;
+      cola.push(NULL);
+      res.push_back(vector<int>());
+    } else {
+      res[nivel].push_back(n->value);
+      for (auto v : n->children) {
+        cola.push(v.second);
+      }
+    }
+  }
+  return res;
 }
 
 int bits(int val) {
-	int cnt = 0;
-	while(val) {
-		cnt++; val /= 2;
-	}
-	return cnt;
+  int cnt = 0;
+  while (val) {
+    cnt++;
+    val /= 2;
+  }
+  return cnt;
 }
 
 size_t sizeForArray(vector<int> &v) {
-	auto m = max_element(v.begin(), v.end());
-	auto b = bits(*m);
-	return (size_t)b * v.size();
+  auto m = max_element(v.begin(), v.end());
+  auto b = bits(*m);
+  return (size_t)b * v.size();
 }
-
--- itemsetsFclaude/test_main.cpp
+++ itemsetsFclaude/test_main.cpp
@@ -8,64 +8,66 @@ using namespace cds_static;
 using namespace cds_utils;
 
 int main() {
-	vector<vector<int>> data;
-	string s;
-	int cnt = 0;
-	
-	while(getline(cin, s)) {
-		data.push_back(vector<int>());
-		stringstream ss(s);
-		int i;
-		while((ss >> i)) {
-			data[cnt].push_back(i);
-		}
-		sort(data[cnt].begin(), data[cnt].end());
-		cnt++;
-	}
+  vector<vector<int>> data;
+  string s;
+  int cnt = 0;
 
-	Tree t = build_tree(data);
-	auto vals = level_values(t);
-	auto vals_label = level_labels(t);
-	size_t size_a = 0;
-	size_t size_b = 0;
-	vector<uint> v;
-	for (size_t i = 0 ; i < vals.size();i++)  {
-	//	cout << vals[i].size() << endl;
-		vector<size_t> r(vals[i].size());
-		for (size_t j = 0 ; j < vals[i].size();j++) {
-			r.push_back(vals[i][j]);
-			v.push_back(vals_label[i][j]);
-		}
-		if (r.size() != 0 ) {
-			RunLengthArray *rla = new RunLengthArray(r);
-			RunLengthBitmap *rlb = new RunLengthBitmap(r);
-			size_a += rla->GetSize();
-			size_b += rlb->GetSize();
-			delete rlb;
-			delete rla;
-		}
-		
-		
-	}
-	Array *r = new Array(v);
-	MapperNone *mn = new MapperNone();
-	BitSequenceBuilder *bsrg = new BitSequenceBuilderRRR(32);
-	WaveletTreeNoptrs *wt = new WaveletTreeNoptrs(*r,bsrg,mn);
-	cout << "Size wt = " << wt->getSize() <<  " - " << (wt->getSize()*1.0)/(1024*1024.0) << endl;
-	cout << "Size using RL array " << size_a << " - " << (1.0*size_a)/(1024*1024.0) << endl;
-	cout << "Size using RL Bitmap " << size_b << " - " << (1.0*size_b)/(1024*1024.0) << endl;
-	size_t si, total = 0;
-	size_t total_len = 0;
-	size_t i = 0;
-	int m = 0;
-	for ( auto v : vals ) {
-		m = max(*max_element(v.begin(), v.end()), m);
-		total_len += v.size();
-		si = sizeForArray(v);
-	//	cout << "Nivel " << i++ << " requiere " << si << endl;
-		total += si;
-	}
-	cout << "Espacio total: " << total << endl;
-	cout << "Total labels: " << total_len << endl;
-	cout << "Labels: " << m << " " << bits(m)*total_len/8 << " " << 1.2*bits(m)*total_len/8.0 << endl;
+  while (getline(cin, s)) {
+    data.push_back(vector<int>());
+    stringstream ss(s);
+    int i;
+    while ((ss >> i)) {
+      data[cnt].push_back(i);
+    }
+    sort(data[cnt].begin(), data[cnt].end());
+    cnt++;
+  }
+
+  Tree t = build_tree(data);
+  auto vals = level_values(t);
+  auto vals_label = level_labels(t);
+  size_t size_a = 0;
+  size_t size_b = 0;
+  vector<uint> v;
+  for (size_t i = 0; i < vals.size(); i++) {
+    //	cout << vals[i].size() << endl;
+    vector<size_t> r(vals[i].size());
+    for (size_t j = 0; j < vals[i].size(); j++) {
+      r.push_back(vals[i][j]);
+      v.push_back(vals_label[i][j]);
+    }
+    if (r.size() != 0) {
+      RunLengthArray *rla = new RunLengthArray(r);
+      RunLengthBitmap *rlb = new RunLengthBitmap(r);
+      size_a += rla->GetSize();
+      size_b += rlb->GetSize();
+      delete rlb;
+      delete rla;
+    }
+  }
+  Array *r = new Array(v);
+  MapperNone *mn = new MapperNone();
+  BitSequenceBuilder *bsrg = new BitSequenceBuilderRRR(32);
+  WaveletTreeNoptrs *wt = new WaveletTreeNoptrs(*r, bsrg, mn);
+  cout << "Size wt = " << wt->getSize() << " - "
+       << (wt->getSize() * 1.0) / (1024 * 1024.0) << endl;
+  cout << "Size using RL array " << size_a << " - "
+       << (1.0 * size_a) / (1024 * 1024.0) << endl;
+  cout << "Size using RL Bitmap " << size_b << " - "
+       << (1.0 * size_b) / (1024 * 1024.0) << endl;
+  size_t si, total = 0;
+  size_t total_len = 0;
+  size_t i = 0;
+  int m = 0;
+  for (auto v : vals) {
+    m = max(*max_element(v.begin(), v.end()), m);
+    total_len += v.size();
+    si = sizeForArray(v);
+    //	cout << "Nivel " << i++ << " requiere " << si << endl;
+    total += si;
+  }
+  cout << "Espacio total: " << total << endl;
+  cout << "Total labels: " << total_len << endl;
+  cout << "Labels: " << m << " " << bits(m) * total_len / 8 << " "
+       << 1.2 * bits(m) * total_len / 8.0 << endl;
 }
